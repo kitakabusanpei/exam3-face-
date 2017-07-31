6434be7236f6490cad11348fbd6fe808a00050c2
渡邉江平 <kouhei.w.56@gmail.com>
2017-07-31 20:19:50 +0900

    first commit

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,48 @@
+class TopicsController < ApplicationController
+  before_action :set_topic, only:[:edit, :update, :destroy]
+  def index
+    @topics = Topic.all
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topics_params)
+    if @topic.save
+      redirect_to root_path, notice: "作品を投稿しました。" # rootにしているので
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @topic.update(topics_params)
+      redirect_to root_path, notice: "作品を更新しました。" # rootにしているので
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @topic.destroy
+    redirect_to root_path, notice: "作品を削除しました。" # rootにしているので
+  end
+
+  private
+  def topics_params
+    params.require(:topic).permit(:content, :photo)
+  end
+
+  def set_topic
+    @topic = Topic.find(params[:id])
+  end
+
+end
