51fec422c4150fd41513687d78600ec7ec4192f8
渡邉江平 <kouhei.w.56@gmail.com>
2017-08-17 20:15:45 +0900

    messe

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,7 +1,27 @@
 class ConversationsController < ApplicationController
+  before_action :authenticate_user!  # ログインしている時のみ有効
   def index
+    @users = User.all
+    @conversations = Conversation.all
   end
 
+  # senderが送り主で`recipientが受取人
   def create
+      # 過去の会話の確認
+    if Conversation.between(params[:sender_id], params[:recipient_id]).present?
+      # 過去の会話の取得
+      @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
+    else
+      # 会話がなかった場合、強制的に作成
+      @conversation = Conversation.create!(conversation_params)
+    end
+    # 会話にひもづくメッセージの一覧画面へ背にさせるs式
+    redirect_to converrsation_messages_path(@conversation)
   end
+
+  private
+  def conversation_params
+    params.permit(:sender_id, :recipient_id)
+  end
+
 end
---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,44 @@
+class MessagesController < ApplicationController
+  before_action do
+    @conversation = Conversation.find(params[:conversation_id])
+  end
+
+  def index
+    # 会話にひもづくメッセージを取得
+    @messages = @conversation.messages
+    # メッセージを１０まで表示、１０より大きければ最新の１０に絞る
+    if @messages.length > 10
+      @over_ten = true
+      @messages = @messages[-10..-1]
+    end
+
+    # 最新のメッセージが自分のユーザーIDでなければ最新のメッセージを既読にする
+    if params[:m]
+      @over_ten = false
+      @messages = @conversation.messages
+    end
+
+    if @messages.last # lastメソッド配列の最後の要素を返しからの時はnilを返す
+      if @messages.last.user_id != current_user.id
+        @messages.last.read = true
+      end
+    end
+
+    # 新規投稿のメッセージ用の変数を作成する
+    @message = @conversation.messages.build
+  end
+
+  def create
+    # HTTPリクエスト上のパラメーターを利用して会話にひもづくメッセージを作成
+    @message = @conversation.messages.build(message_params)
+    # 保存できれば、会話にひもづくメッセージ一覧の画面に遷移する
+    if @message.save
+      redirect_to conversation_messages_path(@conversation)
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :user_id)
+  end
+end
